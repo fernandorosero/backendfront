fba06d13af7802b95c500692f83a67f34d0ee499
fernandorosero <tatofarp@hotmail.com>
2017-04-11 03:58:23 +0200

    Final App

--- src/main/java/com/example/component/TaskComponent.java
+++ src/main/java/com/example/component/TaskComponent.java
@@ -1,5 +1,20 @@
 package com.example.component;
 
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component("tasComponent")
 public class TaskComponent {
 
+	private static Log LOG = LogFactory.getLog(TaskComponent.class);
+	
+	@Scheduled(fixedDelay = 5000)
+	public void toTask(){
+		LOG.info("TIME IS: " + new Date());
+	}
+	
 }
